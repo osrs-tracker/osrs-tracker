caf187cf65fc3ddee15902ac12985f255ed04505
Freek Mencke <info@toxsickproductions.com>
2019-05-06 17:02:16 +0200

    fix OSRS news on Android Pie

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,5 @@ export const environment = {
   production: false,
   API_GEPT: 'https://api.toxsickproductions.com/osrs-tracker',
   // API_GEPT: 'http://localhost:8080',
-  // OLD because no HTTPS available for the rss feed.
-  API_RUNESCAPE_OLD: 'http://services.runescape.com',
   API_RUNESCAPE: 'https://services.runescape.com',
 };
