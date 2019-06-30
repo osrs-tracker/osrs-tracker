70ef0b5a87c0640c4661bfe3394b1e95d61552f2
Freek Mencke <info@toxsickproductions.com>
2019-06-30 10:55:01 +0200

    Update dependencies

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { AppInitializerModule } from './app-initializer.module';
 import { NativeHttpModule } from './native-http/nativeHttp.module';
 import { NerdStatisticsComponent } from './nerd-statistics/nerd-statistics.component';
 
 @NgModule({
-  imports: [AppInitializerModule, NativeHttpModule],
+  imports: [NativeHttpModule],
   declarations: [NerdStatisticsComponent],
   exports: [NerdStatisticsComponent],
 })
